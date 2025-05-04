c4d3447b018640be1f8ce479f1ecd9fd82c466ac
Farhad <108907506+FarhadKhakzad@users.noreply.github.com>
2025-05-04 20:03:37 +0330

    Delete backend directory

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# بارگذاری متغیرهای محیطی از فایل .env
-load_dotenv()
-
-# دریافت مقادیر از .env
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
